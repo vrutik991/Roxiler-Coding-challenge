9c135622fb0e3ba50849952017e51e870a210576
Vrutik Rathod <96432653+vrutik991@users.noreply.github.com>
2025-01-23 11:53:41 +0530

    Styling added

--- Backend/models/amazonaws.js
+++ Backend/models/amazonaws.js
@@ -24,7 +24,7 @@ const amazonawsSchema = new Schema({
         type:String,
     },
     sold:{
-        type:Boolean
+        type:String,
     },
     dateOfSale:{
         type:Date
--- Frontend/transactions/src/App.jsx
+++ Frontend/transactions/src/App.jsx
@@ -6,8 +6,8 @@ import Pagination from './Pagination'
 function App() {
   const [items, setItems] = useState([]);
   const [formData, setformData] = useState({ search: "" });
-  const [currentPage,setcurrentPage] = useState(1);
-  const [postsPerPage,setpostsPerPage] = useState(8); // can be used as an variable.
+  const [currentPage, setcurrentPage] = useState(1);
+  const [postsPerPage, setpostsPerPage] = useState(10); // can be used as an variable.
   const handleSearch = (event) => {
     setformData((currData) => {
       return { ...currData, [event.target.name]: event.target.value };
@@ -23,15 +23,16 @@ function App() {
   const fetchItems = async (query = "") => {
     try {
       const response = await axios.get(`${import.meta.env.VITE_BASE_URL}/transactions`, {
-        params: { 
-          search: query}
+        params: {
+          search: query
+        }
       });
       console.log("Fetched data:", response.data);
       setItems(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
     }
-  }; 
+  };
 
 
   useEffect(() => {
@@ -40,46 +41,48 @@ function App() {
 
   const lastIndex = currentPage * postsPerPage; // 1 * 8 = 8
   const firstIndex = lastIndex - postsPerPage;  // 8 - 8 = 0
-  const current_transactions = items.slice(firstIndex,lastIndex);
-  
+  const current_transactions = items.slice(firstIndex, lastIndex);
+
   return (
     <>
       <form action="" onSubmit={handleSubmit}>
-        <label htmlFor="search">Search: </label>
-        <input style={{ marginRight: "10px" }} type="text" placeholder='Search' id='search' value={formData.search} onChange={handleSearch} name='search' />
+        <label htmlFor="search">Search : </label>
+        <input style={{ marginRight: "10px", marginLeft:"10px" }} type="text" placeholder='Search' id='search' value={formData.search} onChange={handleSearch} name='search' />
 
-        <button>Search</button>
+        <button className='btn btn-success'>Search</button>
       </form>
-
-      <h1>Items List</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <td>id</td>
-            <td>title</td>
-            <td>price</td>
-            <td>description</td>
-            <td>image</td>
-            <td>sold</td>
-            <td>dateofSale</td>
-          </tr>
-        </thead>
-        <tbody>
-          {current_transactions.map((item) => (
-            <tr key={item._id}>
-              <td>{item.id}</td>
-              <td>{item.title}</td>
-              <td>{item.price}</td>
-              <td>{item.description}</td>
-              <td>{item.image}</td>
-              <td>{item.sold}</td>
-              <td>{item.dateOfSale}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <br />
+      <h1>Items List : </h1>
       <br />
-      <Pagination totalposts={items.length} postsperpage={postsPerPage} setcurrentPage={setcurrentPage}></Pagination>      
+      <br />
+      <div className='card-container' style=
+        {{
+          // backgroundColor: "black",
+          display: "flex",
+          flexWrap: "wrap",
+          flex: '200px',
+          gap:"20px"
+          // justifyContent:"space-around",
+        }}>
+        {current_transactions.map((item) => (
+
+          <div className="card" style=
+          {{ 
+            width: "18rem",
+            display:"flex",
+            flexWrap:"wrap",
+            flexDirection:"column"
+            }}>
+            <img src={item.image} className="card-img-top" alt="..." style={{height:"20rem"}}/>
+            <div className="card-body">
+              <h5 className="card-title">{item.title}</h5>
+              {/* <p className="card-text">{item.description}</p> */}
+              <h5>&#8377; {item.price}</h5>
+              <a href="#" className="btn btn-primary">More details</a>
+            </div>
+          </div>
+        ))} </div>
+      <Pagination totalposts={items.length} postsperpage={postsPerPage} setcurrentPage={setcurrentPage}></Pagination>
     </>
   )
 }
